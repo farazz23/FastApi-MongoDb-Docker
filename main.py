7582fa5ca5fe02f77a0e3c3901b78f6cb8a8e76d
theboynextdoor <cman55245@gmail.com>
2025-08-14 00:14:21 +0530

    Added Docker

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from routes import route
+from database import db_config
+
+connect_to_mongo = db_config.connect_to_mongo
+close_connection = db_config.close_connection
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup
+    connect_to_mongo()
+    yield
+    # Shutdown
+    close_connection()
 
 
 app = FastAPI()
--- routes/route.py
+++ routes/route.py
@@ -1,16 +1,17 @@
 from fastapi import APIRouter, HTTPException
-from database import models, db_confg, schema
+from database import db_config, models, schema
 from bson import ObjectId
 from datetime import datetime 
 
+
 router = APIRouter(tags=['Todo'], prefix='/todo')
-collection  = db_confg.collection
-
-
+collection  = db_config.collection
+all_list = schema.all_list
+TODO = models.Todo
 
 
 @router.post("/")
-async def create_post(new_todo : models.Todo):
+async def create_post(new_todo : TODO):
   try:
     response = collection.insert_one(new_todo.dict())
     return {
@@ -26,11 +27,11 @@ async def create_post(new_todo : models.Todo):
 @router.get("/")
 async def get_all_todos():
   data = collection.find();
-  return schema.all_list(data)
+  return all_list(data)
 
 
 @router.put("/{id}")
-async def update_todo(id , updated_todo:models.Todo):
+async def update_todo(id , updated_todo:TODO):
   try:
     is_todo_exist = collection.find_one({"_id":ObjectId(id) , "is_deleted":False })
     if not is_todo_exist:
@@ -48,4 +49,17 @@ async def update_todo(id , updated_todo:models.Todo):
   except Exception as e :
     raise HTTPException(status_code=500 ,
                         detail=f"Something Went Wrong {e}")
+    
+# @router.get("/{id}")
+# async def get_todo(id : str):
+#   try:
+    
+#     todo = collection.find_one({"_id" : ObjectId(id)})
+#     if not todo:
+#       raise HTTPException(status_code=404, 
+#                           detail=f"user not Found ")
+#     return todo   
+#   except Exception as e:
+#     raise HTTPException(status_code=500 ,
+#                         detail=f"Something Went Wrong {e}") 
     
